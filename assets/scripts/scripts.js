bda419fbe0ecdc6b8a3d83dd34c1714813a537d0
james hart <jamesmurphy197197@gmail.com>
2022-11-30 19:51:24 +0000

    [FTR] Add code for displaying dice results and log to console with user messages.

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -6,7 +6,7 @@ for (container of diceContainers) {
     container.addEventListener("mouseup", (e) => {
         e.preventDefault;
         const elements = {};
-        for (child of container.children) {
+        for (child of e.target.parentElement.children) {
             if (child.tagName === 'DIV') {
                 elements.badge = child;
             } else if (child.tagName === 'IMG') {
@@ -35,20 +35,36 @@ for (container of diceContainers) {
     })
 }
 
+//We rolled 2 d20 and 1d12 and got 5, 4, 10. 
+
 getRollButtonElement.addEventListener('click', () => {
-    let totalDice = {
-        total: 0
-    };
+    let diceResults = {};
+    diceResults.total= 0;
+        
     for(input of allDiceInputElements) {
+        if (+input.value === 0) {
+            continue;
+        }
         //find number of faces on dice//
-        const faces = input.getAttribute('data-faces');
-        //apply number of dice chosen
-        totalDice[faces] = +input.value;
+        const faces = +input.getAttribute('data-faces');
         //create array of results for each dice rolled and roll dice//
-        totalDice['rolled'+faces] = [];
-        for(let i=0; i < totalDice[faces]; i++) {
-            totalDice['rolled'+faces].push(Math.floor(Math.random() * +faces) + 1);
+        diceResults[`d${faces}Results`] = [];
+        for(let i=0; i < +input.value; i++) {
+            const result = Math.floor(Math.random() * faces) + 1;
+            diceResults[`d${faces}Results`].push(result);
+            diceResults.total += result
         }
         //display results//
+        diceResults[`d${faces}Messages`] = buildRolledMessage(faces, diceResults[`d${faces}Results`])
+        //reset count//
+        input.value = 0;
     }
+    console.log(diceResults)
 })
+
+function buildRolledMessage(diceType, results) {
+    const rolledMessages = {};
+    rolledMessages.rollingMessage = `Rolling ${results.length}d${diceType}`
+    rolledMessages.resultsMessage = `Rolled (${results.join(' + ')}) total= ${results.reduce((runningTotal, a) => runningTotal + a, 0)}`
+    return rolledMessages;
+}
